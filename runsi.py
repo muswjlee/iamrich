24b8b9a69787d440fa13577a0e83a0d40baaa78d
muswjlee <wjlee@mus-kr.com>
2024-12-26 15:23:52 +0900

    feat : si account - si account

---
+++ runsi.py
@@ -0,0 +1,20 @@
+import mojito
+import pprint
+
+f = open("./mocke.key")
+lines = f.readlines()
+key = lines[0].strip()
+secret = lines[1].strip()
+acc_no = lines[2].strip()
+f.close()
+
+broker = mojito.KoreaInvestment(
+    api_key=key,
+    api_secret=secret,
+    acc_no=acc_no,
+    mock=True
+)
+
+
+resp = broker.fetch_price("005930")
+pprint.pprint(resp)
